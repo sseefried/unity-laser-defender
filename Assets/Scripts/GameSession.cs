bb3686c4b6579be67a47a549e55cf2788ddb3850
Sean Seefried <sean.seefried@gmail.com>
2020-01-22 08:27:16 +1000

    Upgrade to Unity 2018.4.14f1

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameSession : MonoBehaviour
+{
+    [SerializeField] int score = 0; // for debug only
+    
+    void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        int objectCount = FindObjectsOfType(GetType()).Length;
+        if (objectCount > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void IncreaseScore(int amount)
+    {
+        score += amount;
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+
+
+}
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] float loadDelay = 2f; // seconds
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0); // only works if it is first scene
+    }
+
+    public void LoadGame()
+    {
+        // String references aren't great since you could change name of level
+        SceneManager.LoadScene("Game");
+        FindObjectOfType<GameSession>().ResetGame();
+    }
+
+    public void LoadGameOver()
+    {
+        StartCoroutine(WaitAndLoad());
+    }
+     
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private IEnumerator WaitAndLoad()
+    {
+        yield return new WaitForSeconds(loadDelay);
+        SceneManager.LoadScene("Game Over");
+    }
+}
